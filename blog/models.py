f85fac4dbca312868c4e5b1f3455921c5a741b4e
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-20 20:09:32 +0100

    set file structure for html templates and static files

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
 
     def __str__(self):
     
-        return f"The title of this post is {self.title}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -34,4 +34,8 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
-    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
