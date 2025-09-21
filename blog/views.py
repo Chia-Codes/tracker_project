eb7f27f23bf797da2927775f38aac976e962297c
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-21 13:56:40 +0100

    add structure for style.css

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from .models import Post
 
 class Post(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "blog/dashboard.html"
-
+    template_name = "blog/index.html"
+    paginate_by = 6
