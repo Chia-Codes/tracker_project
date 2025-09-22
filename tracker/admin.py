115f0bf359ddc39b46bd77114de913c0bf4a373a
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-22 17:02:29 +0100

    register cyclelog model in admin panel

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CycleLog
 
 # Register your models here.
+
+
+@admin.register(CycleLog)
+class CycleLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'symptom')
+    list_filter = ('user', 'date')
+    search_fields = ('symptom', 'notes')
