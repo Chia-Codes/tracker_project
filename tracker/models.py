2d6367ec8955a036be12cfe82a739be39454d268
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-22 16:56:19 +0100

    create cyclelog model and migrations for tracker

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class CycleLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    symptom = models.CharField(max_length=255, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date} - {self.symptom}"
