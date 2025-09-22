a4832672b05934d535c4810a00a99accac580621
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-22 17:03:19 +0100

    ensure users only see their own logs

--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import CycleLog
 
 # Create your views here.
 
 
 @login_required
 def tracker_view(request):
+    from .models import CycleLog
     return render(request, 'tracker/tracker.html')
