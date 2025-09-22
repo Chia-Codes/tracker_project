cc43149db1e9941335c5768cb593acebb6dbca03
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-22 16:48:32 +0100

    set required login for tracker views

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def tracker_view(request):
     return render(request, 'tracker/tracker.html')
