b95d7ed4372e64eadaff8b1b319f32dff906e5ff
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-25 12:53:16 +0100

    update tracker url pattern, remove duplicate include of 'tracker/tracker'

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tracker_view, name='tracker'),
-    path('tracker/', views.tracker_view, name='tracker'),
     path('tracker/<slug:slug>/', views.tracker_view, name='tracker_detail'),
     path('tracker/log/', views.cycle_log_form_view, name='cycle_log_form'),
 ]
