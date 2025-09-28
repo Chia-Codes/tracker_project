1cba5383c8bc2dabf89140752542b7527539c1e7
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-28 13:48:09 +0100

    Use base64-encoded credentials from env

--- manage.py
+++ manage.py
@@ -2,6 +2,25 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('trackher_user_cycle_log')
+
+sales = SHEET.worksheet('Sheet1')
+
+data = sales.get_all_values()
+
+print(data)
 
 
 def main():
