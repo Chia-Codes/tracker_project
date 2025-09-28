8a44144d300429a5b9e0611c5f5832b8ac5c498d
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-09-28 16:42:51 +0100

    create forms.py and set comment accessibility for logged in users

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+# Add form for comments
+from .models import Comment
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
