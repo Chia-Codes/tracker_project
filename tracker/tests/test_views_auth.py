2b9c92db7ece7c17f9b05f592fbdf1fbe4f44a57
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-10-04 17:09:48 +0100

    create and run authorised views test

---
+++ tracker/tests/test_views_auth.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class AuthViewsTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.u = User.objects.create_user(username='alice', password='pw')
+
+    def test_submit_log_requires_login(self):
+        res = self.client.post(reverse('submit_log'), data={})
+        self.assertEqual(res.status_code, 302)  # redirected to login
+
+    def test_export_requires_login(self):
+        res = self.client.get(reverse('export_csv'))
+        self.assertEqual(res.status_code, 302)
