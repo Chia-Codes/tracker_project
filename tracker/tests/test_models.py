af59a7c4ab00264d899a39d0c27fef53d839b73b
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-10-04 17:05:52 +0100

    create cycle log models test and run test

---
+++ tracker/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from datetime import date
+from tracker.models import CycleLog
+
+
+class CycleLogModelTests(TestCase):
+    def setUp(self):
+        self.u = User.objects.create_user(username='alice', password='pw')
+
+    def test_unique_per_user_per_day(self):
+        CycleLog.objects.create(user=self.u, date=date(2025, 9, 25), symptom='cramps', notes='')
+        with self.assertRaises(IntegrityError):
+            CycleLog.objects.create(user=self.u, date=date(2025, 9, 25), symptom='headache', notes='x')
